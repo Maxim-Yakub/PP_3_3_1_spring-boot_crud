920734d1eef6980e071579ba41a3af416aab8d24
Work <79774488441@ya.ru>
2022-08-08 15:51:03 +0300

    Initial commit

---
+++ src/main/java/com/example/pp_3_3_1_springboot_crud/Pp331SpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.pp_3_3_1_springboot_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp331SpringBootCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pp331SpringBootCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pp_3_3_1_springboot_crud/Pp331SpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pp_3_3_1_springboot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp331SpringBootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
