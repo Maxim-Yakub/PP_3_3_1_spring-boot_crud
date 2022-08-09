8cc3dea70f7fe652dcc0c4988e4b610c6d4cb681
Work <79774488441@ya.ru>
2022-08-09 08:47:26 +0300

    before check

---
+++ src/main/java/com/example/pp_3_3_1_springboot_crud/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.example.pp_3_3_1_springboot_crud.controller;
+
+import com.example.pp_3_3_1_springboot_crud.model.User;
+import com.example.pp_3_3_1_springboot_crud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String main(Model model) {
+        model.addAttribute("users",userService.getAll());
+        return "users";
+    }
+
+    @GetMapping("/search")
+    public String searchUser(@RequestParam(value = "keyword") String keyword, Model model) {
+        model.addAttribute("users", userService.search(keyword));
+        return "searchUser";
+    }
+
+    @GetMapping("/newUser")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        return "newUser";
+    }
+
+    @PostMapping()
+    public String createUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.get(id));
+        return "editUser";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") long id) {
+        userService.update(id, user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        userService.delete(id);
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/com/example/pp_3_3_1_springboot_crud/service/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package com.example.pp_3_3_1_springboot_crud.service;
+
+import com.example.pp_3_3_1_springboot_crud.model.User;
+import com.example.pp_3_3_1_springboot_crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final
+    UserRepository repository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public void save (User user) {
+        repository.save(user);
+    }
+
+    @Transactional
+    public List<User> getAll () {
+        return (List<User>) repository.findAll();
+    }
+
+    @Transactional
+    public User get(Long id) {
+        return repository.findById(id).get();
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Transactional
+    public void update(Long id, User updatedUser){
+        User userToBeUpdated = get(id);
+        userToBeUpdated.setName(updatedUser.getName());
+        userToBeUpdated.setLastName(updatedUser.getLastName());
+        userToBeUpdated.setEmail(updatedUser.getEmail());
+    }
+
+    @Transactional
+    public List<User> search(String keyword) {
+        return repository.search(keyword);
+    }
+}
