cc5cbe51859b249e300a90c6a3fec39283b1a071
Work <79774488441@ya.ru>
2022-08-10 09:56:39 +0300

    @repo

--- src/main/java/com/example/pp_3_3_1_springboot_crud/repository/UserRepository.java
+++ src/main/java/com/example/pp_3_3_1_springboot_crud/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.example.pp_3_3_1_springboot_crud.model.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     @Query(value="SELECT user from User user where user.name LIKE  CONCAT('%',:keyword,'%')"
             + "OR user.email LIKE CONCAT('%',:keyword,'%')"
